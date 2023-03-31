4e114fed2d2b1dc32c22216a3bd1cce3d0f02e8a
aayush262 <aayushlama26@gmail.com>
2023-04-01 01:20:43 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,59 @@
-import React, { useRef, useEffect } from 'react';
-import WebViewer from '@pdftron/webviewer';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import WebViewer from "@pdftron/webviewer";
+import "./App.css";
 
 const App = () => {
   const viewer = useRef(null);
+  const [webViewerInstance, setWebViewerInstance] = useState(null);
+  const [files, setFiles] = useState(null);
 
-  // if using a class, equivalent of componentDidMount 
+  const handleChange = (e) => {
+    setFiles(e.target.files[0])
+  }
+
   useEffect(() => {
-    WebViewer(
-      {
-        path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
-      },
-      viewer.current,
-    ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+    if (files) {
+      WebViewer(
+        {
+          path: "/webviewer/lib",
+          initialDoc: URL.createObjectURL(files),
+        },
+        viewer.current
+      ).then((instance) => {
+        setWebViewerInstance(instance);
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
-
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
+        const { annotationManager } = instance.Core;
       });
-    });
-  }, []);
+    }
+  }, [files]);
 
+  const handleButtonClick = () => {
+    const { annotationManager } = webViewerInstance.Core;
+
+    const fieldManager = annotationManager.getFieldManager();
+    const fields = fieldManager.getFields();
+    const payload = fields.map((field) => {
+      const { x1, y1, x2, y2 } = field.widgets[0].getRect();
+      return {
+        input_data: field.getValue(),
+        x1,
+        y1,
+        x2,
+        y2,
+        font_size: field.font.calculatedSize,
+        fieldName: field.name,
+        fieldType: field.getFieldType()
+      };
+    });
+    console.log(payload);
+  };
+  
   return (
-    <div className="App">
-      <div className="header">React sample</div>
-      <div className="webviewer" ref={viewer}></div>
-    </div>
+    <>
+      <input type="file" onChange={handleChange}/>
+      {files && <div className="webviewer" ref={viewer}></div>}
+      {files && <button onClick={handleButtonClick}>Get Form Values</button>}
+    </>
   );
 };
 
